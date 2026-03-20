b9ff72622bdcdf85aadfb9097af05244f08294a5
neshdras <samonasan@gmail.com>
2026-03-20 16:19:45 +0100

    Refactor color handling and button state management in the game board

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,8 @@ import { useScore } from './store/storeScore'
 function App() {
 
   const [board,setBoard] = useState(Array(5).fill().map(row => new Array(5).fill("")))
-  const [isDisable,setIsDisable]=useState(false)
-  const couleurs = ["red","blue","yellow","green","orange"]
-  const [colors, setColor] = useState(couleurs[Math.floor(Math.random() * couleurs.length)])
+  const colorsTab = ["red","blue","yellow","green","orange"]
+  const [color, setColor] = useState(colorsTab[Math.floor(Math.random() * colorsTab.length)])
   // Constante des scores
     const scoreX = useScore((state) => state.scoreX)
     const scoreY = useScore((state) => state.scoreY)
@@ -17,20 +16,14 @@ function App() {
     const resetY = useScore((state) => state.resetScoreY)
 
   function changeColor(){
-    setColor(couleurs[Math.floor(Math.random() * couleurs.length)])
+    setColor(colorsTab[Math.floor(Math.random() * colorsTab.length)])
 
   }
   
   function handleClick(e,i,j){
-    e.target.className=colors
-    e.target.disabled=true
     const newBoard=[...board]
-
-    newBoard[i][j]=colors
-
+    newBoard[i][j]=color
     setBoard(newBoard)
-    console.log("Click",i,j)
-    console.log(newBoard)
     changeColor()
   }
 
@@ -40,11 +33,11 @@ function App() {
 
       <div className="board">
         {board.map((cell,i)=>(cell.map((cell2,j)=>
-          <button disabled={isDisable} name="pad" key={j} className={`cell`} onClick={(e)=>handleClick(e,i,j)}>
+          <button disabled={board[i][j] ? true : false} name="pad" key={j} className={`cell ${board[i][j]}`} onClick={(e)=>handleClick(e,i,j)}>
           
           </button>
         )))}
-        <p>Vous jouer la couleur : {colors}</p>
+        <p>Vous jouer la couleur : {color}</p>
       </div>
     </>
   )
