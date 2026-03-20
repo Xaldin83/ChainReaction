dec1ef35ada8aeecdd120102737d4584176aa1dc
Xaldin83 <mariuslucas.pichon@gmail.com>
2026-03-20 13:40:00 +0100

    Début d'utilisation d'une matrice

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,35 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [board,setBoard] = useState(Array(5).fill(Array(5).fill(null)))
+  const [isDisable,setIsDisable]=useState(false)
+  const couleurs = ["red","blue","yellow","green","orange"]
+
+
+  function changeColor(i,j){
+    const newBoard = [...board]
+    newBoard[i][j]="Click"
+    console.log(newBoard[i][j])
+    setBoard(newBoard)
+  }
+  
+  function handleClick(i,j){
+    console.log("Click",i,j)
+    
+    changeColor(i,j)
+  }
 
   return (
     <>
       <h1>Chain Reaction</h1>
+
+      <div className="board">
+        {board.map((cell,i)=>(cell.map((cell2,j)=>
+          <button disabled={isDisable} name="pad" key={j} className="cell" onClick={()=>handleClick(i,j)}>
+          {board[i][j]}
+          </button>
+        )))}
+      </div>
     </>
   )
 }
