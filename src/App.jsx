bac054d3bbe325990000fd3dae855fbe6d9b778f
Xaldin83 <mariuslucas.pichon@gmail.com>
2026-03-23 09:26:56 +0100

    On a rajouter la fonction de calcul des points

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ function App() {
   const [board,setBoard] = useState(Array(5).fill().map(row => new Array(5).fill("")))
   const colorsTab = ["red","blue","yellow","green","orange"]
   const [color, setColor] = useState(colorsTab[Math.floor(Math.random() * colorsTab.length)])
+  const [isX, setIsX]=useState(true)
   // Constante des scores
     const scoreX = useScore((state) => state.scoreX)
     const scoreY = useScore((state) => state.scoreY)
@@ -24,12 +25,35 @@ function App() {
   function checkCombination(i,j){
     // Fonction qui vérifie les différentes combinaisons afin de vérifier s'il y a un une combinaison
     let cpt = 0
-    for(let val = 0;val<board.length;val++) //Vérification ligne
+    for(let val = 0;val<board.length;val++){ //Vérification ligne
       if(board[val][j]===board[i][j])
         cpt++
       else
         if(cpt<3)
-          cpt=0
+          cpt=0}
+    if(cpt!=0){
+      for(let k =0;k<cpt-2;k++){
+        if(isX)
+            increaseScoreX()
+        else
+          increaseScoreY()}
+    }
+    console.log(cpt)
+    cpt=0
+    for(let val = 0;val<board.length;val++){ //Vérification ligne
+      if(board[i][val]===board[i][j])
+        cpt++
+      else
+        if(cpt<3)
+          cpt=0}
+    if(cpt!=0){
+      for(let k =0;k<cpt-2;k++){
+        if(isX)
+          increaseScoreX()
+        else
+          increaseScoreY()}
+    }
+    console.log(cpt)
   }
   
   function handleClick(e,i,j){
@@ -37,12 +61,14 @@ function App() {
     newBoard[i][j]=color
     setBoard(newBoard)
     changeColor()
+    checkCombination(i,j)
   }
 
   return (
     <>
       <h1>Chain Reaction</h1>
 
+      <p>Score Joueur 1 : {scoreX}<br/>Score Joueur 2 : {scoreY}</p>
       <div className="board">
         {board.map((cell,i)=>(cell.map((cell2,j)=>
           <button disabled={board[i][j] ? true : false} name="pad" key={j} className={`cell ${board[i][j]}`} onClick={(e)=>handleClick(e,i,j)}>
